d241ba93adf5f952d2f166a99ce7545b49b64905
krt465 <karthikeyan.raja2@sky.uk>
2026-05-07 13:23:27 +0100

    gh #34 updated build_docs.sh and install.sh

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -83,6 +83,7 @@ Options:
 
 Examples:
   $0 serve          Serve your MkDocs site on localhost:8000
+  $0 serve -a 0.0.0.0:<port no>   Serve your MkDocs site on a server and bind to all interfaces
   $0 build          Build your MkDocs site into the 'site' directory
   $0 deploy         <version> - Deploy your MkDocs site to GitHub Pages
   $0 set-default    <version> - Set the default version of the deployed docs
